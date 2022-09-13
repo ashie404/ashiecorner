938a529f52ef9e200117dae548631736ef66f3c0
June <22226164+juniebyte@users.noreply.github.com>
2022-09-13 14:14:42 -0500

    so much stuff

---
+++ js/site.js
@@ -0,0 +1,23 @@
+// this poorly written javascript brought to you by a gay cat
+
+// randomized text stuff hehe :3
+var r_text = new Array ();
+r_text[0] = "nyanyanya!";
+r_text[1] = "sdfasdklfgsdfgsgoinrfoenlvbd";
+r_text[2] = "Ashie was here";
+r_text[3] = "Zenith was here";
+r_text[4] = "Visual Studio Code is certainly the program of all time";
+r_text[5] = "Reject human, embrace meow";
+r_text[6] = "meow :3";
+r_text[7] = "incrediblegaming";
+r_text[8] = "pawwwwsssssss,,,,,";
+r_text[9] = "mawwwwsssssss,,,,,";
+r_text[10] = "sonic robo blsat 7";
+r_text[11] = "Fun fact: I invented the sun";
+r_text[12] = "<3 Zenith"
+r_text[13] = "Puerto Amongalas"
+r_text[14] = "awawawawa,,"
+r_text[15] = "The included spork is certainly the utensil of all time"
+var i = Math.floor(16*Math.random())
+
+$(".scroll-text").html(r_text[i]);

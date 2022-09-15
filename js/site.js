6ee5437e69ef4cc3e682cbc45c1a2a8f05ca7e4c
June <22226164+juniebyte@users.noreply.github.com>
2022-09-15 18:28:24 -0500

    funny blur out of focus

--- js/site.js
+++ js/site.js
@@ -46,10 +46,12 @@ function navButton() {
         $(".sidebar").css('left', '');
         $(".navButton").css('left', '');
         $(".navButton img").attr('src', 'img/menu.svg');
+        $(".page-content").attr('id', '');
     } else {
         $(".sidebar").css('left', '0px');
         $(".navButton").css('left', '259px');
         $(".navButton img").attr('src', 'img/close.svg');
+        $(".page-content").attr('id', 'navOpen');
     }
     navOpen = !navOpen;
 }
